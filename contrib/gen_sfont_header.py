d539cb2f17941e0f98c8d942dfe07cd79ab46b04
clach04 <clach04@localhost>
2010-11-25 09:46:45 -0800

    Added newlines in binary/hex array. Added option to specify name in header via command line. Added demo code to output header.

--- contrib/gen_sfont_header.py
+++ contrib/gen_sfont_header.py
@@ -74,6 +74,105 @@ extern "C" {
 %(hex_byte_str_list)s
 };
 
+#ifdef SFONT_INC_FONT_MAIN
+
+/*
+** example way to build:
+**
+**  cc `sdl-config --cflags` `sdl-config --libs` -DSFONT_INC_FONT_MAIN SFont.c %(hdr_name)s.h -o %(hdr_name)s
+**
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "SDL.h"
+#include "SFont.h"
+
+#include "SDL_image.h"
+
+SDL_Surface *screen;
+
+void init_SDL()
+{
+    if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+    fprintf(stderr,
+        "Couldn't initialize SDL: %%s\\n", SDL_GetError());
+    exit(1);
+    }
+    atexit(SDL_Quit);            /* Clean up on exit */
+    
+    /* Initialize the display */
+    screen = SDL_SetVideoMode(640, 480, 0, 16);
+    if ( screen == NULL ) {
+    fprintf(stderr, "Couldn't set %%dx%%dx%%d video mode: %%s\\n",
+        640, 480, 16, SDL_GetError());
+    exit(1);
+    }
+    
+    /* Set the window manager title bar */
+    SDL_WM_SetCaption("SFont Font Viewer", "SFontViewer");
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Event event;
+    SDL_Surface *font_bitmap_surface=NULL;
+    SFont_Font* Font;
+
+    font_bitmap_surface = SDL_LoadBMP("24P_Copperplate_Blue.bmp"); /* NOTE only needs base SDL, no need for SDL_Image */
+    if(!font_bitmap_surface)
+    {
+        fprintf(stderr, "Using default");
+        font_bitmap_surface = get_%(hdr_name)s_font();
+    }
+    
+    if(!font_bitmap_surface)
+    {
+        fprintf(stderr, "An error occured while loading the font.");
+        exit(1);
+    }
+    
+    Font = SFont_InitFont(font_bitmap_surface);
+    if(!Font)
+    {
+        fprintf(stderr, "An error occured while setting up font.");
+        exit(1);
+    }
+
+
+    init_SDL();
+
+    if (argc > 2) {
+    /* a simple text blit to (0/0) */
+    SFont_Write(screen, Font, 0, 0, argv[2]);
+    } else {
+    SFont_Write(screen, Font, 0, 0, argv[1]);
+    SFont_WriteCenter(screen,Font,60,"SFont Font Viewer");
+    SFont_WriteCenter(screen,Font,300,"This text is for testing purposes.");
+    SFont_WriteCenter(screen,Font,360,"This one, too <>%%&-~|/_#+");
+    }
+
+    /* Update the screen */
+    SDL_UpdateRect(screen, 0, 0, 0, 0);
+    /* Let the user time to look at the font */
+    SDL_EventState( SDL_KEYUP, SDL_IGNORE );
+    SDL_EventState( SDL_ACTIVEEVENT, SDL_IGNORE );
+    SDL_EventState( SDL_MOUSEMOTION, SDL_IGNORE );
+    SDL_EventState( SDL_SYSWMEVENT, SDL_IGNORE );
+    SDL_WaitEvent(&event);
+
+    /* Don't forget to free our font */
+    SFont_FreeFont(Font);
+    
+    /* Bye */
+    exit(0);
+}
+
+
+#endif /* SFONT_INC_FONT_MAIN */
+
 #ifdef __cplusplus
 }
 #endif
@@ -89,7 +188,14 @@ def bmp2header(bmp_filename, name=None, use_c99_types=False):
     f.close()
     byte_len = len(file_bytes)
 
-    file_bytes_str = ['0x%02x'%ord(x) for x in file_bytes]  # list comprehension python 2.4 feature
+    def dumb_wrap_calc(x):
+        if x % 13 == 0:
+            return 1
+        else:
+            return 0
+        
+    #file_bytes_str = ['0x%02x%s' % (ord(x), '/* %d */'%y) for y, x in enumerate(file_bytes)]  # list comprehension python 2.4 feature
+    file_bytes_str = ['0x%02x%s' % (ord(x), dumb_wrap_calc(y)*'\n') for y, x in enumerate(file_bytes)]  # list comprehension python 2.4 feature
 
     hdr_name = name
     if use_c99_types:
@@ -113,13 +219,17 @@ def main(argv=None):
         print ''
         print 'Error missing file name'
         print ''
-        print 'Usage: gen_sfont_header.py file_name.bmp'
+        print 'Usage: gen_sfont_header.py file_name.bmp [name]'
         print ''
         return 1
+        
     name, extn = os.path.splitext(os.path.basename(bmp_filename))
     if extn.lower() != '.bmp':
         print 'WARNING! file name does not contain BMP.'
-    name = '_'+name
+    try:
+        name = argv[2]
+    except IndexError:
+        name = '_'+name
     bmp2header(bmp_filename, name)
     
     return 0
